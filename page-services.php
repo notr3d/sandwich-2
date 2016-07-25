153faafbf6b6beb66da20ca581d0b20f35f4289e
notr3d <lxmrrr@gmail.com>
2016-07-25 19:03:34 +0300

    25.07

--- page-services.php
+++ page-services.php
@@ -1,42 +1,48 @@
-<?php get_header(); ?>			
-<div class="site-nav">
-	<div class="page-heading">
-		<div class="page-heading__wrapper lcw">
-			<h1 class="page-heading__header"><span>Услуги</span></h1>
-			<p class="page-heading__text">Помимо изготовления ограждающих конструкций на заказ, завод «Новый Формат» готов предложить вам полный комплекс услуг «Сэндвич-панели под ключ» или любую интересующую вас дополнительную услугу в отдельности.</p>
-		</div>
-	</div>
-	<div class="site-nav-submenu lcw" id="submenu-3">
-		<div class="site-nav-submenu__item">
-			<a href="delivery" class="site-nav-submenu__inner">
-				<img src="" alt="">
-				<h3>Доставка</h3>
-			</a>
-		</div>
-		<div class="site-nav-submenu__item">
-			<a href="packing" class="site-nav-submenu__inner">
-				<img src="" alt="">
-				<h3>Упаковка</h3>
-			</a>
-		</div>
-		<div class="site-nav-submenu__item">
-			<a href="fillet" class="site-nav-submenu__inner">
-				<img src="" alt="">
-				<h3>Раскладка</h3>
-			</a>
-		</div>
-		<div class="site-nav-submenu__item">
-			<a href="installation" class="site-nav-submenu__inner">
-				<img src="" alt="">
-				<h3>Монтаж</h3>
-			</a>
-		</div>
-		<div class="site-nav-submenu__item">
-			<a href="accessories" class="site-nav-submenu__inner">
-				<img src="" alt="">
-				<h3>Комплектация</h3>
-			</a>
-		</div>					
-	</div>
+<?php get_header(); ?>	
+<div class="services">		
+    <div class="site-nav">
+        <div class="page-heading">
+            <div class="page-heading__wrapper lcw">
+                <div class="page-heading__container">
+                    <h1 class="page-heading__header"><span>Услуги</span></h1>
+                    <p class="page-heading__text">Помимо изготовления ограждающих конструкций на заказ, завод «Новый Формат» готов предложить вам полный комплекс услуг «Сэндвич-панели под ключ» или любую интересующую вас дополнительную услугу в отдельности.</p>
+                </div>
+            </div>
+        </div>
+        <div class="site-nav-submenu lcw">
+            <div class="site-nav-submenu__wrapper">
+                <div class="site-nav-submenu__item">
+                    <a href="delivery" class="site-nav-submenu__inner">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/services/items/1.png" alt="">
+                        <h3>Доставка</h3>
+                    </a>
+                </div>
+                <div class="site-nav-submenu__item">
+                    <a href="packing" class="site-nav-submenu__inner">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/services/items/2.png" alt="">
+                        <h3>Упаковка</h3>
+                    </a>
+                </div>
+                <div class="site-nav-submenu__item">
+                    <a href="fillet" class="site-nav-submenu__inner">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/services/items/3.png" alt="">
+                        <h3>Раскладка</h3>
+                    </a>
+                </div>
+                <div class="site-nav-submenu__item">
+                    <a href="installation" class="site-nav-submenu__inner">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/services/items/4.png" alt="">
+                        <h3>Монтаж</h3>
+                    </a>
+                </div>
+                <div class="site-nav-submenu__item">
+                    <a href="accessories" class="site-nav-submenu__inner">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/services/items/5.png" alt="">
+                        <h3>Комплектация</h3>
+                    </a>
+                </div>
+            </div>					
+        </div>
+    </div>
 </div>
 <?php get_footer(); ?>
